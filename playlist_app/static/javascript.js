a437987ed20b46554744400370cea85312072626
danielSmirn0v <dansmirskate@gmail.com>
2023-03-26 18:12:22 -0400

    commented out js

--- playlist_app/static/javascript.js
+++ playlist_app/static/javascript.js
@@ -1,15 +1,17 @@
 
-function delete_song(song_id) {
-    var xhr = new XMLHttpRequest();
-    console.log('bouta run')
-    xhr.open('DELETE', `/playlist/${song_id}/delete_song`);
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-            console.log('Song deleted successfully');
-        } else {
-            console.error('Error deleting song');
-        }
-    };
-    console.log('this did not wokr')
-    xhr.send();
-}
+// function delete_song(song_id) {
+//     var xhr = new XMLHttpRequest();
+//     console.log('bouta run')
+//     xhr.open('DELETE', `/playlist/${song_id}/delete_song`);
+//     xhr.onload = function() {
+//         if (xhr.status === 200) {
+//             console.log('Song deleted successfully');
+//         } else {
+//             console.error('Error deleting song');
+//         }
+//     };
+//     console.log('this did not wokr')
+//     xhr.send();
+// }
+
+// figure out how to do this with out re-routing page
