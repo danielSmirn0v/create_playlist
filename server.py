3e5dbee85974c27f9fe8bee92228351f9b7b49b3
danielSmirn0v <dansmirskate@gmail.com>
2023-04-25 18:09:38 -0400

    added some depnedcies

--- server.py
+++ server.py
@@ -4,9 +4,14 @@ from playlist_app import app
 
 from playlist_app.controllers import users,playlists
 
+import os
 
 
 
 
-if __name__ == '__main__' :
-    app.run( debug = True)
+
+# if __name__ == '__main__' :
+#     app.run( debug = True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
